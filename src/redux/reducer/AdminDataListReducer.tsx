084b20a9bfb1f2871428abe5856e0480598b1c04
mfaizk <faizk123485@gmail.com>
2023-04-11 22:02:32 +0530

    admin reducer introduced

--- src/redux/reducer/AdminDataListReducer.tsx
+++ src/redux/reducer/AdminDataListReducer.tsx
@@ -1,12 +1,28 @@
-// import {createSlice} from '@reduxjs/toolkit';
-// interface data{
+import {createSlice} from '@reduxjs/toolkit';
+export interface productData {
+  address: string;
+  category: string;
+  description: string;
+  imageURL: string;
+  latitude: string;
+  longitude: string;
+}
+const initialState: productData = {
+  address: '',
+  category: '',
+  description: '',
+  imageURL: '',
+  latitude: '',
+  longitude: '',
+};
 
-// }
+const AdminDataListSlice = createSlice({
+  name: 'adminDataList',
+  initialState: [initialState],
+  reducers: {
+    initialDataEntry: state => {},
+  },
+});
 
-// export interface listState extends Array<> {
-
-// }
-
-// const AdminDataListReducer = createSlice({
-//   name: 'adminDataList',
-// });
+export const {initialDataEntry} = AdminDataListSlice.actions;
+export default AdminDataListSlice.reducer;
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -2,12 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import authSlice from '../reducer/AuthReducer';
 import loadingslice from '../reducer/LoadingReducer';
 import themeSlice from '../reducer/ThemeReducer';
-
+import AdminDataListSlice from '../reducer/AdminDataListReducer';
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     loading: loadingslice,
     theme: themeSlice,
+    adminDataList: AdminDataListSlice,
   },
   middleware(getDefaultMiddleware) {
     return getDefaultMiddleware({serializableCheck: false});
